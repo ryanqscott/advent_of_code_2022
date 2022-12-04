0d9ae363ad8e9edb7539c8b7f0b88c8e5ca41574
ryanqscott <rqscott97@gmail.com>
2022-12-04 18:29:24 +0200

    day four!

---
+++ day_four/part_two.js
@@ -0,0 +1,21 @@
+const {readFileSync} = require('fs');
+
+const contents = readFileSync(__dirname + '/input', 'utf-8');
+
+let cleanupPair = contents.split('\n');
+let numOfOverlappingAssignments = 0;
+for (let assignment of cleanupPair) {
+    if (assignment.length < 1) {// ignoring any blank lines in input
+        continue;
+    }
+    let assignments = assignment.split(',');
+    let firstAssignment = assignments[0].split('-');
+    let secondAssignment = assignments[1].split('-');
+    // checking for any intersections of the range
+    if (parseInt(firstAssignment[1]) >= parseInt(secondAssignment[0]) && parseInt(firstAssignment[1]) <= parseInt(secondAssignment[1])) {
+        numOfOverlappingAssignments++;
+    } else if (parseInt(secondAssignment[1]) >= parseInt(firstAssignment[0]) && parseInt(secondAssignment[1]) <= parseInt(firstAssignment[1])) {
+        numOfOverlappingAssignments++;
+    }
+}
+console.log(numOfOverlappingAssignments);
